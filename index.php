4709964953a0a273be4d9cc4dd4f291c6a0044db
Sam Ferkler <70289628+SF940064@users.noreply.github.com>
2023-12-12 15:34:11 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            margin: 0;
+            background-color: rgb(4, 4, 85);
+        }
+
+        h1 {
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            text-align: left;
+            color: rgb(252, 252, 226);
+            font-size: medium;
+            width: 450px;
+        }
+
+        h2 {
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            text-align: left;
+            color: rgb(252, 252, 226);
+            font-size: small;
+            width: 450px;
+        }
+
+        p {
+            font-family: Georgia, 'Times New Roman', Times, serif;
+            text-align: left;
+            color: rgb(252, 252, 226);
+            font-size: smaller;
+            width: 450px;
+        }
+
+        .vb-headshot {
+            width: 300px;
+            height: 450px;
+            object-position: right;
+            /* Add more styles as needed */
+        }
+    </style>
+</head>
+<body>
+    <h1> Samuel Ferkler </h1>
+    <img class="vb-headshot" src="/Users/samuelferkler/GitHub Portfolio/samuelferkler" alt="headshot of samuel ferkler">
+    <h2> About Me </h2>
+    <p>
+        Welcome to my portfolio! I'm Samuel Ferkler, a 24-year-old recent graduate from West Chester University
+        where I earned a Bachelor's of Science in Computer Science and a minor in Statistics.
+    </p>
+
+    <p>
+        During my time at West Chester University, I fell in love with the elegance of writing software and coding projects. 
+     I'm always thankful for my professors, my peers, and the memories made in the classroom. 
+ </p>  
+
+ <p>
+        After graduating in May 2023, I've been working on solo projects, including game and web design (like this portfolio!) 
+     to keep my coding skills sharp and challenge myself in unfamiliar territories to become a better programmer. 
+ </p>
+ 
+ <p>
+        My career goal is to find ways to help people through programming, whether that's designing a new website to aid people 
+     in navigating the internet more easily or working on life-changing software. The opportunities to help are endless 
+     in this line of work, and helping others has always been my lifelong aspiration.
+ </p> 
+     
+ <p>
+        I grew up in Ridley Park, Pennsylvania, raised by the best parents anyone could ask for. 
+     During my childhood, I loved playing sports at the park with my friends and riding our bicycles all over town. 
+     This love for being outdoors and playing sports stayed with me throughout my upbringing and even into becoming a varsity high school athlete, 
+     specializing in Lacrosse and Football. These sports were something I held onto even at the next level, college. 
+     I was recruited to play Division 3 College Football at King's College in Wilkes-Barre, PA. 
+     My time on the team was short-lived, however, as I struggled to find a balance between football and schoolwork. 
+     Sometimes I regret walking away, but I'll cherish the memories that football gave me forever. 
+ </p>
+ 
+ <p>
+        Once I settled into college academics and found a groove, I discovered my new passion: coding. 
+     During the spring semester of my freshman year at King's College, 
+     I couldn't stop working on my coding projects. 
+     I quickly realized that there is no better feeling in the world than writing code that does exactly what you wanted it to do. 
+     To this day, I still feel the same. I spent hours daily in my professor, Chad Hogg's, office. 
+     I couldn't put my computer down and was always ready to learn more. 
+     With Professor Hogg's guidance, I became a very passionate, idealistic coder. 
+ </p>
+ 
+ <p>
+        After my freshman year at King's, I decided to transfer to West Chester University as I didn't have many friends 
+     at King's and wanted to be closer to home. I settled into West Chester very easily. 
+     I had plenty of people that I knew from towns over that I started hanging out with daily and are now some of my closest friends. 
+     During my 3rd year at West Chester, I was eager to find a bartending job in town. 
+     Never having bartended before, this was very challenging, as I didn't realize that you should have bartending experience to become a bartender. 
+     I eventually got a job barbacking at Ram's Head Bar & Grille (Best bar ever, if you know, you know). 
+     Two months at Ram's Head got me enough experience in the industry to become a bartender next door at Mas Mexicali. 
+     During my time at Mas, I unlocked another passion of mine: bartending. 
+ </p>
+ 
+ <p>
+        What I love most about bartending is the conversations that you have. I'll always hang onto certain conversations I have with some guests. 
+     Conversations that may have helped them, may have helped me, or were just friendly conversations to make their stay at my bar more enjoyable. 
+     I recently left Mas Mexicali, but I'll always be thankful for the memories I made and becoming the best bartender I can be there. 
+ </p>
+
+    <p>
+        I am currently behind the bar at Flanigan's Boathouse in Conshohocken, which, in my opinion, has the best bar food I have ever experienced.
+        Come check me out behind the bar at Flanigan's on the weekends and enjoy some great beer and great food!
+    </p>
+
+</body>
+</html>
